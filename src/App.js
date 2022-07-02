063cffcdc2d10873c0311fd0d7fb5d73fdd9fd97
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-07-02 07:05:40 -0500

    fix: Navigation Bar styles with template strings

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
           </TooltipComponent>
         </div>
         {activeMenu ? <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">Sidebar</div> : <div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>}
-        <div className={activeMenu ? 'dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full' : 'dark:bg-main-bg bg-main-bg min-h-screen w-full flex-2'}></div>
+        <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}></div>
       </div>
     </BrowserRouter>
   );
