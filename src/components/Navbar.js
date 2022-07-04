c34ff61eb11f3ea22598d83afdea5cf0f9ebf864
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-07-04 05:10:04 -0500

    feat: add other 2 icons to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,6 +26,8 @@ const Navbar = () => {
       <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu />} />
       <div className="flex">
         <NavButton title="Cart" customFunc={() => handleClick('cart')} color="blue" icon={<FiShoppingCart />} />
+        <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color="blue" icon={<BsChatLeft />} />
+        <NavButton title="Notifications" dotColor="#03C9D7" customFunc={() => handleClick('notification')} color="blue" icon={<RiNotification3Line />} />
       </div>
     </div>
   );
